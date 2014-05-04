6dab37ebee588843ca4ba2cb455fa1cc366d0918
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-05-04 18:49:37 -0400

    changes in deploy and staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,14 @@ lock '3.2.1'
 
 set :application, 'creositios.com'
 set :repo_url, 'git@github.com:patojimenez/creositios.com.git'
-set :branch, proc { 'git rev-parse --abbrev-ref HEAD'.chomp }
+#set :branch, proc { 'git rev-parse --abbrev-ref HEAD'.chomp }
+set :branch, "master"
 set :deploy_via, :copy
 set :deploy_to, '/var/www/creositios.com'
 set :scm, :git
 set :pty, true
 set :linked_dirs, %w{uploads}
+
 
 namespace :deploy do
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,7 @@ set :stage, :staging
 server 'creositios.com', roles: %w{web app db}
 
 set :user, 'root'
+set :use_sudo, false
 
 set :ssh_options, {
    user: fetch(:user)
